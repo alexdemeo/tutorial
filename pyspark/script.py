ebcfaeff6fa7e9b646ea090b84bc925a8e75ed9c
Alex DeMeo <alex.demeo129@gmail.com>
2024-03-28 18:12:12 -0700

    test

--- pyspark/script.py
+++ pyspark/script.py
@@ -18,6 +18,7 @@ def run_job(spark, final_output_table):
       timestamp(date_trunc(b.confirm_time, 'DD')) AS "booking_date",
       b.earning_adjustment_commission_rate AS "commission_rate",
       b.receipt_payment_type AS "payment_type",
+      b.reward_id AS "reward_id",
       c.*,
     FROM
       bookings_30_days b
